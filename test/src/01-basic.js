88c86ae596511315ee6a8c2b65dfa4772f08d963
黒田 英二 <eiji-kuroda@dac.co.jp>
2016-12-07 18:31:37 +0900

    draft

--- test/src/01-basic.js
+++ test/src/01-basic.js
@@ -9,16 +9,40 @@ class Ctrl1 {
   tagNameOfTome() { return 'div'; } // eslint-disable-line class-methods-use-this
 }
 
+class Ctrl2 {
+  tagNameOfPlayer() { return 'img'; } // eslint-disable-line class-methods-use-this
+  tagNameOfTome() { return 'span'; } // eslint-disable-line class-methods-use-this
+}
+
 describe('Basic', () => {
-  beforeEach(() => {
-  });
-  it('load success', (done) => {
+  it('dom check', (done) => {
     const ctl = new Ctrl1();
     const j2d = new Json2Dom(ctl);
     const url = '../data/simple.js';
     j2d.load(url).then((dom) => {
-      // expect(dom).to.deep.equal(answer);
-      dom.should.contain('div[role="player"]');
+      expect(dom).to.have.attribute('role', 'wrapper');
+      expect(dom).to.contain('img[role="loader"]');
+      expect(dom).to.contain('img[role="btnClose"]');
+      expect(dom).to.contain('div[role="passback"]');
+      expect(dom).to.contain('div[role="video"]');
+      expect(dom).to.contain('div[role="video"] div[role="player"]');
+      expect(dom).to.contain('div[role="video"] div[role="tome"]');
+      expect(dom).to.contain('div[role="video"] img[role="btnFull"]');
+      expect(dom).to.contain('div[role="video"] img[role="btnDetail"]');
+      expect(dom).to.contain('div[role="video"] img[role="btnReplay"]');
+      done();
+    }).catch((error) => {
+      done(error);
+    });
+  });
+
+  it('dynamic tag', (done) => {
+    const ctl = new Ctrl2();
+    const j2d = new Json2Dom(ctl);
+    const url = '../data/simple.js';
+    j2d.load(url).then((dom) => {
+      expect(dom).to.contain('div[role="video"] img[role="player"]');
+      expect(dom).to.contain('div[role="video"] span[role="tome"]');
       done();
     }).catch((error) => {
       done(error);
